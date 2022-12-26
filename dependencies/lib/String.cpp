94a9e3374e7b03b88f4cb358a1a13eea16b659b8
yoqub-davlatov <yoqub.dj03@gmail.com>
2022-12-26 23:46:17 +0300

    Fixed String::sort

--- dependencies/lib/String.cpp
+++ dependencies/lib/String.cpp
@@ -180,11 +180,6 @@ void String::sort(String *arr, int size)
                 arr[j + 1] = temp;
             }
         }
-        for (int i = 0; i < 5; i++)
-        {
-            std::cout << arr[i] << ' ';
-        }
-        std::cout << std::endl;
     }
 }
 
